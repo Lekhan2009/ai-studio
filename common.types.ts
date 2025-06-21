bf485e9a7a81d31e491907f811283c17d67f21d8
Lekhan2009 <152042199+Lekhan2009@users.noreply.github.com>
2025-06-21 22:48:25 +0530

    Update common.types.ts

--- common.types.ts
+++ common.types.ts
@@ -1,30 +1,32 @@
-import { User, Session } from 'next-auth'
+// common.types.ts
 
-export type FormState = {
-    title: string;
-    description: string;
-    image: string;
-    liveSiteUrl: string;
-    githubUrl: string;
-    category: string;
-};
+import { User, Session } from 'next-auth';
+
+export interface FormState {
+  title: string;
+  description: string;
+  image: string;
+  liveSiteUrl: string;
+  githubUrl: string;
+  category: string;
+}
 
 export interface ProjectInterface {
-    title: string;
-    description: string;
-    image: string;
-    liveSiteUrl: string;
-    githubUrl: string;
-    category: string;
+  title: string;
+  description: string;
+  image: string;
+  liveSiteUrl: string;
+  githubUrl: string;
+  category: string;
+  _id: string;
+  id: string;
+  createdBy: {
+    name: string;
+    email: string;
+    avatarUrl: string;
     _id: string;
     id: string;
-    createdBy: {
-      name: string;
-      email: string;
-      avatarUrl: string;
-      _id: string;
-      id: string;
-    };
+  };
 }
 
 export interface ProjectForm {
@@ -38,30 +40,28 @@ export interface ProjectForm {
 }
 
 export interface UserProfile {
-    _id: string;
-    id: string;
-    name: string;
-    email: string;
-    description: string | null;
-    avatarUrl: string;
-    githubUrl: string | null;
-    linkedinUrl: string | null;
-    projects: {
-      edges: { node: ProjectInterface }[];
-      pageInfo: {
-        hasPreviousPage: boolean;
-        hasNextPage: boolean;
-        startCursor: string;
-        endCursor: string;
-      };
+  _id: string;
+  id: string;
+  name: string;
+  email: string;
+  description: string | null;
+  avatarUrl: string;
+  githubUrl: string | null;
+  linkedinUrl: string | null;
+  projects: {
+    edges: { node: ProjectInterface }[];
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+      endCursor: string;
     };
+  };
 }
 
 export interface SessionInterface extends Session {
   user: User & {
     id: string;
-    name: string;
-    email: string;
     avatarUrl: string;
   };
 }
